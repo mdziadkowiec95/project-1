2c26e51eef4885e520f613fd12fea4bcca68d491
Piotr Dziadkowiec <piotrdziadkowiec@MacBook-Air-Piotr-2.local>
2024-02-23 19:13:35 +0100

    problem1

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+const search = document.getElementById("search");
+search.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const inputValue = event.target.value;
+  console.log(inputValue);
+});
+
+/*
+
+Chce zrobic 2 rzeczy banalnie proste ale nie umiem:
+
+1.
+---
+Prosta opcja zeby wyszukiwana zawartosc wyswietlic w console.log po wcisnieciu enter... :/
+
+2.
+---
+Dać ten inputfield  na srodek ekranu horyzontalnie i wertykalnie.
+
+Pozdro!
+*/
