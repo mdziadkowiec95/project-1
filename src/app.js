8c950ca5e81f207b385e65df996259da4ad70770
Piotr Dziadkowiec <piotrdziadkowiec@MacBook-Air-Piotr-2.local>
2024-02-24 15:43:27 +0100

    weather API searching by name

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // Najprosciej to tak zrobic:
 const search = document.getElementById("search-form");
+// const input = document.getElementById("search");
 
 search.addEventListener("submit", function (event) {
   // blokujemy domyslne zachowanie formularza (bo domyslnie przeladuje strone po kliknieciu w submit)
@@ -7,8 +8,27 @@ search.addEventListener("submit", function (event) {
 
   // tworzymy obiekt FormData na podstawie event.target (czyli formularza)
   const formData = new FormData(event.target);
+
+  // console.log(formData);
   // pobiernie wartosci z inputa
   const inputValue = formData.get("search");
+  // for (const pair of formData.entries()) {
+  //   console.log(pair[0], pair[1]);
+  // }
 
-  console.log(inputValue);
+  // console.log(inputValue);
+
+  const request = new XMLHttpRequest();
+  request.open(
+    "GET",
+    `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=fc46e9714ae267890941c2e8d3350790`
+  );
+  request.send();
+
+  request.addEventListener("load", function () {
+    // console.log(this.responseText);
+    const data = JSON.parse(this.responseText);
+    console.log(data);
+    console.log(data.name, " - ", data.main.temp, "st. C");
+  });
 });
