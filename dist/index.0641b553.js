7d38459013e83afd1c0795c93d7d20dc39e83c99
Piotr Dziadkowiec <piotrdziadkowiec@MacBook-Air-Piotr-2.local>
2024-02-28 19:46:05 +0100

    error

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -604,25 +604,46 @@ const renderCard = function(data) {
     cardContainer.insertAdjacentHTML("beforeend", html);
 // cardContainer.classList.add("invisible");
 };
+const renderErr = function(data) {
+    cardContainer.innerHTML = "";
+    const html = `
+  <div class="card" style="width: 18rem">
+          <img src="https://cdn.pixabay.com/photo/2017/02/12/21/29/false-2061131_1280.png" class="card-img-top" alt="..." /> 
+          <div class="card-body">
+            <h5 class="card-title">CITY NOT FOUND \u{1F937}\u{1F3FC}\u{200D}\u{2642}\u{FE0F}</h5>
+            <p class="card-text">${data}
+            </p>
+            
+          </div>
+        </div>
+  `;
+    cardContainer.insertAdjacentHTML("beforeend", html);
+// cardContainer.classList.add("invisible");
+};
 search.addEventListener("submit", async function(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const inputValue = formData.get("search");
-    async function request() {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=fc46e9714ae267890941c2e8d3350790`);
-        const city = await response.json();
-        console.log(city);
-        return city;
-    // const clearThis = await function (target) {
-    //   event.target.value = "";
-    // };
-    // clearThis(this);
+    try {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const inputValue = formData.get("search");
+        async function request() {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=fc46e9714ae267890941c2e8d3350790`);
+            const city = await response.json();
+            console.log(city);
+            return city;
+        // const clearThis = await function (target) {
+        //   event.target.value = "";
+        // };
+        // clearThis(this);
+        }
+        const data = await request(inputValue);
+        console.log("2", data);
+        renderCard(data);
+        console.log(inputValue);
+        input.value = "";
+    } catch (err) {
+        console.log(err.message);
+        renderErr(err.message);
     }
-    const data = await request(inputValue);
-    console.log("2", data);
-    renderCard(data);
-    console.log(inputValue);
-    input.value = "";
 // input.classList.add("invisible");
 });
 
--- src/app.js
+++ src/app.js
@@ -25,31 +25,54 @@ const renderCard = function (data) {
   cardContainer.insertAdjacentHTML("beforeend", html);
   // cardContainer.classList.add("invisible");
 };
+const renderErr = function (data) {
+  cardContainer.innerHTML = "";
+  const html = `
+  <div class="card" style="width: 18rem">
+          <img src="https://cdn.pixabay.com/photo/2017/02/12/21/29/false-2061131_1280.png" class="card-img-top" alt="..." /> 
+          <div class="card-body">
+            <h5 class="card-title">CITY NOT FOUND 🤷🏼‍♂️</h5>
+            <p class="card-text">${data}
+            </p>
+            
+          </div>
+        </div>
+  `;
+
+  cardContainer.insertAdjacentHTML("beforeend", html);
+  // cardContainer.classList.add("invisible");
+};
 
 search.addEventListener("submit", async function (event) {
-  event.preventDefault();
-  const formData = new FormData(event.target);
+  try {
+    event.preventDefault();
+    const formData = new FormData(event.target);
 
-  const inputValue = formData.get("search");
+    const inputValue = formData.get("search");
 
-  async function request() {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=fc46e9714ae267890941c2e8d3350790`
-    );
-    const city = await response.json();
-    console.log(city);
-    return city;
+    async function request() {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=fc46e9714ae267890941c2e8d3350790`
+      );
+      const city = await response.json();
+      console.log(city);
+      return city;
 
-    // const clearThis = await function (target) {
-    //   event.target.value = "";
-    // };
-    // clearThis(this);
+      // const clearThis = await function (target) {
+      //   event.target.value = "";
+      // };
+      // clearThis(this);
+    }
+    const data = await request(inputValue);
+    console.log("2", data);
+    renderCard(data);
+
+    console.log(inputValue);
+    input.value = "";
+  } catch (err) {
+    console.log(err.message);
+    renderErr(err.message);
   }
-  const data = await request(inputValue);
-  console.log("2", data);
-  renderCard(data);
-  console.log(inputValue);
-  input.value = "";
 
   // input.classList.add("invisible");
 });
